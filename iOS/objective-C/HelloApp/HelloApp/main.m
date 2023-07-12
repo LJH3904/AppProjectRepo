9de55e4d220f22cd688eab0b331e4e3b6586a524
Jehyeon Lee <dasom8899@naver.com>
2023-07-12 17:50:08 +0900

    옵젝씨
    배운내용 강의파일

---
+++ iOS/objective-C/HelloApp/HelloApp/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  HelloApp
+//
+//  Created by LJh on 2023/07/12.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // Setup code that might create autoreleased objects goes here.
+  }
+  return NSApplicationMain(argc, argv);
+}
