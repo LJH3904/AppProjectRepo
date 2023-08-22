49373d9c047c15e819e82e938dee739737022d32
Jehyeon Lee <dasom8899@naver.com>
2023-08-22 11:42:23 +0900

    Create JobPostingListView.swift

---
+++ iOS/SwiftUI/Project/ToyProject/Checking/Checking/View/JobPostingListView.swift
@@ -0,0 +1,20 @@
+//
+//  JobPostingListView.swift
+//  S_nowManCustomer
+//
+//  Created by LJh on 2023/08/22.
+//
+
+import SwiftUI
+
+struct JobPostingListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JobPostingListView_Previews: PreviewProvider {
+    static var previews: some View {
+        JobPostingListView()
+    }
+}
